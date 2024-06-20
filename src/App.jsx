f3c399a9e50a781c75a24dfda8701172fa6b236b
Bernardo Alvarez Braga <bernardo.alvarezbraga@gmail.com>
2024-06-20 00:17:54 -0300

    Update general CSS and for some components
    Updated:
    - Index.css
    - App.css
    - Navbar.module.css
    - Footer.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Styles
 import "./App.css";
 
+//Pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+//Components
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+
+//Styles
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <NavLink to="/" className={styles.brand}>
+          Mini <span>Blog</span>
+        </NavLink>
+        <ul className={styles.links_list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
